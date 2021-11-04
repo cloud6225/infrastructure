7d7e541e74aa8d0169fc6e62774c88f38eef2734
Mrudul Ladhwe <ladhwe.m@northeastern.edu>
2021-11-04 14:58:11 -0400

    s3 and ec2 commit

--- variables.tf
+++ variables.tf
@@ -4,21 +4,62 @@ variable "region" {
 }
 
 variable "profile" {
-  default = "dev"
+  type = string
+  description = "profile"
 }
 
 variable "subnet_azs" {
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
-variable "vpc_name"{
-    default = "main"
+variable "vpc_name" {
+  default = "main"
 }
 
-variable "cidr_block_vpc"{
+variable "cidr_block_vpc" {
   default = "10.0.0.0/16"
 }
 
-variable "cidr_block_route"{
-}
+variable "cidr_block_route" {
   default = "0.0.0.0/0"
+}
+
+variable "mysubnet_group" {
+  default = "subnet_group"
+}
+
+variable "db_pg" {
+  default = "pg"
+}
+
+variable "s3_domain"{
+	default = "mrudulladhwe"
+}
+
+variable "s3_name"{
+	default = "bucket57"
+}
+
+variable "rds_password" {
+  type = string
+  description = "rds password"
+}
+
+variable "postgres_username" {
+  type = string
+  description = "postgres username"
+}
+
+variable "rds_name" {
+  default = "csye6225"
+}
+
+variable "port" {
+  default = "5432"
+}
+
+variable "ec2_key" {
+  type        = string
+  description = "ec2 key pair"
+}
+
