51af667dbb2f111096dea0294ffbdd911d17b67c
Mrudul Ladhwe <ladhwe.m@northeastern.edu>
2021-11-18 16:06:04 -0500

    make bucket access private

--- vpc.tf
+++ vpc.tf
@@ -290,6 +290,15 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 #creating IAM role
 resource "aws_iam_role" "role" {
   name = "EC2-CSYE6225"
